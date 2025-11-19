8ca9c4287de80e0bb6883017eca1a033c95a7b95
pryharinbohdan <j@k>
2025-11-19 12:32:54 +0000

    переробив основну частину програми (перевірити)

--- main.cpp
+++ main.cpp
@@ -558,26 +558,11 @@ Linked2List<T>::~Linked2List() {
 
 
 
-/////////////////////////// ОСНОВНА ПРОГРАМА ///////////////////////////////
+// [Весь попередній код класу Linked2List залишається без змін]
+// Тут має бути ваш повний клас Linked2List з усіма методами
 
-// СИСТЕМА УПРАВЛІННЯ ПЛЕЙЛИСТОМ МУЗИКИ
-/*
-struct song {
-    char name[100];
-    char author[100];
-    int howLong;
-    song() : name(""), author(""), howLong(0) {}
-    song(char* name, char* author, const int howLong) : howLong(howLong) {
-        std::copy(name, name+100, this -> name);
-        std::copy(author, author+100, this->author);
-    }
-};*/
+/////////////////////////// ДОПОМІЖНІ ФУНКЦІЇ ///////////////////////////////
 
-// ToDo LIST СПИСОК ЗАВДАНЬ
-
-#include <ctime>
-
-// Допоміжна функція порівняння рядків (повертає true якщо рівні)
 bool strcmp_equal(const char* a, const char* b) {
     size_t i = 0;
     while (a[i] != '\0' && b[i] != '\0') {
@@ -587,7 +572,6 @@ bool strcmp_equal(const char* a, const char* b) {
     return a[i] == b[i];
 }
 
-// Допоміжна функція копіювання рядка
 void strcpy_custom(char* dest, const char* src) {
     size_t i = 0;
     while (src[i] != '\0') {
@@ -597,135 +581,311 @@ void strcpy_custom(char* dest, const char* src) {
     dest[i] = '\0';
 }
 
-// Допоміжна функція визначення довжини рядка
 size_t strlen_custom(const char* str) {
     size_t len = 0;
     while (str[len] != '\0') ++len;
     return len;
 }
 
-struct Task {
-    char* description;
-    size_t n;
-    time_t whenAdded;
+/////////////////////////// СТРУКТУРА ПІСНІ ///////////////////////////////
+
+struct Song {
+    char* name;
+    char* author;
+    int duration; // в секундах
     
-    Task() : n(1), whenAdded(time(nullptr)) {
-        description = new char[n];
-        description[0] = '\0';
+    Song() : name(nullptr), author(nullptr), duration(0) {
+        name = new char[1];
+        name[0] = '\0';
+        author = new char[1];
+        author[0] = '\0';
     }
     
-    Task(const char* desc) : whenAdded(time(nullptr)) {
-        n = strlen_custom(desc) + 1;
-        description = new char[n];
-        strcpy_custom(description, desc);
+    Song(const char* n, const char* a, int d) : duration(d) {
+        size_t len = strlen_custom(n) + 1;
+        name = new char[len];
+        strcpy_custom(name, n);
+        
+        len = strlen_custom(a) + 1;
+        author = new char[len];
+        strcpy_custom(author, a);
     }
     
-    // Конструктор копіювання
-    Task(const Task& other) : n(other.n), whenAdded(other.whenAdded) {
-        description = new char[n];
-        strcpy_custom(description, other.description);
+    Song(const Song& other) : duration(other.duration) {
+        size_t len = strlen_custom(other.name) + 1;
+        name = new char[len];
+        strcpy_custom(name, other.name);
+        
+        len = strlen_custom(other.author) + 1;
+        author = new char[len];
+        strcpy_custom(author, other.author);
     }
     
-    // Оператор присвоєння
-    Task& operator=(const Task& other) {
-        if(this != &other) {
-            delete[] description;
-            n = other.n;
-            whenAdded = other.whenAdded;
-            description = new char[n];
-            strcpy_custom(description, other.description);
+    Song& operator=(const Song& other) {
+        if (this != &other) {
+            delete[] name;
+            delete[] author;
+            
+            size_t len = strlen_custom(other.name) + 1;
+            name = new char[len];
+            strcpy_custom(name, other.name);
+            
+            len = strlen_custom(other.author) + 1;
+            author = new char[len];
+            strcpy_custom(author, other.author);
+            
+            duration = other.duration;
         }
         return *this;
     }
     
+    bool operator==(const Song& other) const {
+        return strcmp_equal(name, other.name) && 
+               strcmp_equal(author, other.author) && 
+               duration == other.duration;
+    }
+    
+    bool operator!=(const Song& other) const {
+        return !(*this == other);
+    }
+
+    
     // Оператор < (порівняння часу)
-    bool operator<(const Task& other) const {
-        return whenAdded < other.whenAdded;
+    bool operator<(const Song& other) const {
+        return duration < other.duration;
     }
     
     // Оператор > (порівняння часу)
-    bool operator>(const Task& other) const {
-        return whenAdded > other.whenAdded;
-    }
-    
-    // Оператор == (рівність часу та рядка)
-    bool operator==(const Task& other) const {
-        if(whenAdded != other.whenAdded) return false;
-        return strcmp_equal(description, other.description);
-    }
-    
-    // Оператор != 
-    bool operator!=(const Task& other) const {
-        return !(*this == other);
+    bool operator>(const Song& other) const {
+        return duration > other.duration;
     }
     
     // Оператор <= (менше за часом або рівні)
-    bool operator<=(const Task& other) const {
+    bool operator<=(const Song& other) const {
         return (*this < other) || (*this == other);
     }
     
     // Оператор >= (більше за часом або рівні)
-    bool operator>=(const Task& other) const {
+    bool operator>=(const Song& other) const {
         return (*this > other) || (*this == other);
     }
     
-    ~Task() {
-        delete[] description;
+    ~Song() {
+        delete[] name;
+        delete[] author;
     }
 };
 
-bool up(Task& a, Task& b) {
-    return a <= b;
+/////////////////////////// ПРЕДИКАТИ ТА КОМПАРАТОРИ ///////////////////////////////
+
+bool compare_by_duration(Song& a, Song& b) {
+    return a.duration <= b.duration;
 }
 
-bool down(Task& a, Task& b) {
-    return a >= b;
+bool is_long_song(Song& s) {
+    return s.duration > 300; // більше 5 хвилин
+}
+
+bool is_short_song(Song& s) {
+    return s.duration < 180; // менше 3 хвилин
+}
+
+/////////////////////////// ОСНОВНА ПРОГРАМА ///////////////////////////////
+
+void print_menu() {
+    std::cout << "\n========== МЕНЮ ПЛЕЙЛИСТА ==========\n";
+    std::cout << "1  - Додати пісню в кінець\n";
+    std::cout << "2  - Додати пісню на початок\n";
+    std::cout << "3  - Показати всі пісні\n";
+    std::cout << "4  - Показати в зворотному порядку\n";
+    std::cout << "5  - Видалити останню пісню\n";
+    std::cout << "6  - Видалити першу пісню\n";
+    std::cout << "7  - Знайти пісню за назвою\n";
+    std::cout << "8  - Видалити довгі пісні (>5хв)\n";
+    std::cout << "9  - Сортувати за тривалістю\n";
+    std::cout << "10 - Розмір плейлиста\n";
+    std::cout << "11 - Очистити плейлист\n";
+    std::cout << "12 - Обміняти з іншим плейлистом\n";
+    std::cout << "13 - Злити з іншим плейлистом\n";
+    std::cout << "0  - Вихід\n";
+    std::cout << "====================================\n";
+    std::cout << "Ваш вибір: ";
+}
+
+void print_song(const Song& s, int num) {
+    std::cout << num << ". " << s.name << " - " << s.author 
+              << " (" << s.duration / 60 << ":";
+    if (s.duration % 60 < 10) {
+        std::cout << "0";
+    }
+    std::cout << s.duration % 60 << ")\n";
 }
 
 int main() {
-    Linked2List<Task> ls;
+    Linked2List<Song> playlist1;
+    Linked2List<Song> playlist2;
     
-    bool isExit = false;
+    // Додамо тестові пісні
+    playlist1.push_back(Song("Bohemian Rhapsody", "Queen", 354));
+    playlist1.push_back(Song("Imagine", "John Lennon", 183));
+    playlist1.push_back(Song("Stairway to Heaven", "Led Zeppelin", 482));
     
-    do {
-        ls.sort(down);
-        std::cout << "Ваш ToDo список справ:" << std::endl << std::endl;
-        int i = 1;
-        for (auto it = ls.begin(); it != ls.end(); ++it) {
-            std::cout << "-------------------------------" << std::endl;
-            std::cout << i << " " << it->description << std::endl;
-            ++i;
-        }
-        std::cout << "-------------------------------" << std::endl;
-        std::cout << std::endl << "Щоб додати нову справу, напишіть \"new\"" << std::endl;
-        std::cout << "Щоб видалити справу, напишіть \"rm\"" << std::endl;
-        std::cout << "Щоб вийти, напишіть \"exit\"" << std::endl;
+    int choice;
+    bool running = true;
+    
+    while (running) {
+        print_menu();
+        std::cin >> choice;
+        std::cin.ignore();
         
-        char command[100];
-        std::cin >> command;
-        
-        if (strcmp_equal(command, "exit")) {
-            isExit = true;
-        } else if (strcmp_equal(command, "new")) {
-            std::cout << "Введіть опис нової справи: ";
-            std::cin.ignore();
-            char desc[256];
-            std::cin.getline(desc, 256);
-            ls.push_back(Task(desc));
-        } else if (strcmp_equal(command, "rm")) {
-            std::cout << "Введіть номер справи для видалення: ";
-            int num;
-            std::cin >> num;
-            auto it = ls.begin();
-            if (num > 0 && num <= ls.size()) {
-                for (int j = 1; j < num; ++j) ++it;
-                ls.erase(it);
-            } else {
-                std::cout << "Невірний номер!" << std::endl;
+        switch (choice) {
+            case 1: { // push_back
+                char name[256], author[256];
+                int dur;
+                std::cout << "Назва: ";
+                std::cin.getline(name, 256);
+                std::cout << "Автор: ";
+                std::cin.getline(author, 256);
+                std::cout << "Тривалість (сек): ";
+                std::cin >> dur;
+                playlist1.push_back(Song(name, author, dur));
+                std::cout << "Пісню додано!\n";
+                break;
             }
+            
+            case 2: { // push_front
+                char name[256], author[256];
+                int dur;
+                std::cout << "Назва: ";
+                std::cin.getline(name, 256);
+                std::cout << "Автор: ";
+                std::cin.getline(author, 256);
+                std::cout << "Тривалість (сек): ";
+                std::cin >> dur;
+                playlist1.push_front(Song(name, author, dur));
+                std::cout << "Пісню додано на початок!\n";
+                break;
+            }
+            
+            case 3: { // Показати всі (використання iterator)
+                if (playlist1.empty()) {
+                    std::cout << "Плейлист порожній!\n";
+                } else {
+                    std::cout << "\n--- ПЛЕЙЛИСТ ---\n";
+                    int i = 1;
+                    for (auto it = playlist1.begin(); it != playlist1.end(); ++it) {
+                        print_song(*it, i++);
+                    }
+                }
+                break;
+            }
+            
+            case 4: { // Показати в зворотному порядку (reverse_iterator)
+                if (playlist1.empty()) {
+                    std::cout << "Плейлист порожній!\n";
+                } else {
+                    std::cout << "\n--- ЗВОРОТНИЙ ПОРЯДОК ---\n";
+                    int i = 1;
+                    for (auto it = playlist1.rbegin(); it != playlist1.rend(); ++it) {
+                        print_song(*it, i++);
+                    }
+                }
+                break;
+            }
+            
+            case 5: { // pop_back
+                if (!playlist1.empty()) {
+                    playlist1.pop_back();
+                    std::cout << "Останню пісню видалено!\n";
+                } else {
+                    std::cout << "Плейлист порожній!\n";
+                }
+                break;
+            }
+            
+            case 6: { // pop_front
+                if (!playlist1.empty()) {
+                    playlist1.pop_front();
+                    std::cout << "Першу пісню видалено!\n";
+                } else {
+                    std::cout << "Плейлист порожній!\n";
+                }
+                break;
+            }
+            
+            case 7: { // find
+                char name[256];
+                std::cout << "Введіть назву пісні: ";
+                std::cin.getline(name, 256);
+                
+                bool found = false;
+                int pos = 1;
+                for (auto it = playlist1.begin(); it != playlist1.end(); ++it, ++pos) {
+                    if (strcmp_equal(it->name, name)) {
+                        std::cout << "Знайдено!\n";
+                        print_song(*it, pos);
+                        found = true;
+                        break;
+                    }
+                }
+                if (!found) {
+                    std::cout << "Пісню не знайдено!\n";
+                }
+                break;
+            }
+            
+            case 8: { // remove за предикатом
+                playlist1.remove(is_long_song);
+                std::cout << "Довгі пісні видалено!\n";
+                break;
+            }
+            
+            case 9: { // sort
+                playlist1.sort(compare_by_duration);
+                std::cout << "Плейлист відсортовано!\n";
+                break;
+            }
+            
+            case 10: { // size
+                std::cout << "Кількість пісень: " << playlist1.size() << "\n";
+                break;
+            }
+            
+            case 11: { // clear
+                playlist1.clear();
+                std::cout << "Плейлист очищено!\n";
+                break;
+            }
+            
+            case 12: { // swap
+                std::cout << "Обмін з другим плейлистом...\n";
+                playlist1.swap(playlist2);
+                std::cout << "Плейлисти обмінено!\n";
+                break;
+            }
+            
+            case 13: { // merge
+                if (playlist2.empty()) {
+                    std::cout << "Другий плейлист порожній!\n";
+                } else {
+                    playlist1.sort(compare_by_duration);
+                    playlist2.sort(compare_by_duration);
+                    playlist1.merge(playlist2);
+                    std::cout << "Плейлисти злито!\n";
+                }
+                break;
+            }
+            
+            case 0: {
+                running = false;
+                std::cout << "До побачення!\n";
+                break;
+            }
+            
+            default:
+                std::cout << "Невірний вибір!\n";
         }
-        
-    } while (!isExit);
+    }
     
     return 0;
 }
